5f3ba4010fb6e21fc654e2d0882e715fcabade50
kat-billings <katbillings235@gmail.com>
2021-08-03 20:05:49 -0500

    Completed bonus missions

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then( function(json) {
             const div = document.getElementById("container");
+            json.sort(function (a, b) {
+                return b.hoursInSpace - a.hoursInSpace;
+            });
             for(let i=0; i<json.length; i++){
+                let green = ''
+                if (json[i].active === true){
+                    green = 'class="green"';
+                }
                 div.innerHTML += `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${json[i].firstName} ${json[i].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${json[i].hoursInSpace}</li>
-                           <li>Active: ${json[i].active}</li>
+                           <li ${green}>Active: <span>${json[i].active}</li>
                            <li>Skills: ${(json[i].skills).join(', ')}</li>
                         </ul>
                     </div>
@@ -17,6 +24,9 @@ window.addEventListener("load", function() {
                 </div>
                 `;
             }
+            div.innerHTML += `
+                <h2>Astronaut Count: ${json.length}</h2>
+            `;
         });
     });
 });
